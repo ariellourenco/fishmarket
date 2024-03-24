7969434bb351de0d2662e24ad062527ce4d323bd
Ariel Lourenco <ariellourenco@users.noreply.github.com>
2024-03-24 16:38:07 -0300

    Add my first integration test.

--- tests/FishMarket.Tests/UserApiTests.cs
+++ tests/FishMarket.Tests/UserApiTests.cs
@@ -1,10 +1,30 @@
+using FishMarket.Api.Endpoints;
+
 namespace FishMarket.Tests;
 
 public sealed class UserApiTests
 {
-    [Fact]
-    public void CanCreateUser()
-    {
+    private const string BaseUrl = "/users";
 
+    [Fact]
+    public async Task CanCreateUser()
+    {
+        // Arrange
+        await using var app = new TestServerFactory();
+        await using var db = app.CreateDbContext();
+
+        var client = app.CreateClient();
+
+        // Act
+        var response = await client.PostAsJsonAsync(BaseUrl, new UserInfo { Email = "todouser@test.com", Password = "@pwd" });
+
+        // Assert
+        Assert.True(response.IsSuccessStatusCode);
+
+        var user = db.Users.Single();
+
+        Assert.NotNull(user);
+        Assert.Equal("todouser@test.com", user.Email);
+        Assert.Equal("todouser@test.com", user.UserName);
     }
 }
